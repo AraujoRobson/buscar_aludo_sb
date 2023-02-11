3e3b3ab54519e9cfadda87700e4554c937f4c04f
Robson Araujo <roobson.araujo02@gmail.com>
2023-02-11 11:51:15 -0300

    primeiro commit

---
+++ src/main/java/br/edu/unoesc/buscar_aluno/BuscarAlunoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.buscar_aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscarAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscarAlunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unoesc/buscar_aluno/BuscarAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.buscar_aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscarAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
